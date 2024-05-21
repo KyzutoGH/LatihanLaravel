10db597c7568136c4e3eb826a317a89898cf9cea
KyzutoGH - Muhammad Ikhsan <iksancjr@gmail.com>
2024-05-21 14:26:12 +0700

    Coba

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\SiswaController;
+use App\Http\Controllers\Weba;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,3 +22,16 @@ Route::get('/siswa', [SiswaController::class, 'index']);
 Route::get('/siswa/list', [SiswaController::class, 'list']);
 Route::get('/siswa/create', [SiswaController::class, 'create']); 
 Route::post('/siswa', [SiswaController::class, 'simpan']);
+
+//Routing WEBA
+Route::get('coba', [Weba::class, 'index']);
+
+Route::get('coba/create', [Weba::class, 'create']);
+Route::post('coba', [Weba::class, 'store']);
+
+Route::get('coba{id}', [Weba::class, 'show']);
+
+Route::get('coba/{id}/edit', [Weba::class, 'edit']);
+Route::put('coba/{id}', [Weba::class, 'update']);
+
+Route::delete('coba/{id}', [Weba::class, 'delete']);
